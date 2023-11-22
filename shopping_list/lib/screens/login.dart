4c72a03bd5c0a1d07e168d8bc26ec1c13913113c
Edward-Salim <edwardsalim29@gmail.com>
2023-11-22 12:06:08 +0700

    Menyelesaikan Tutorial 9

--- shopping_list/lib/screens/login.dart
+++ shopping_list/lib/screens/login.dart
@@ -68,7 +68,7 @@ class _LoginPageState extends State<LoginPage> {
 
                 // Cek kredensial
                 final response = await request.login(
-                    "http://edward-salim-tutorial.pbp.cs.ui.ac.id/auth/login/",
+                    "http://127.0.0.1:8000/auth/login/",
                     {
                       'username': username,
                       'password': password,
--- shopping_list/lib/screens/shoplist_form.dart
+++ shopping_list/lib/screens/shoplist_form.dart
@@ -124,7 +124,7 @@ class _ShopFormPageState extends State<ShopFormPage> {
                           if (_formKey.currentState!.validate()) {
                             // Kirim ke Django dan tunggu respons
                             final response = await request.postJson(
-                                "http://edward-salim-tutorial.pbp.cs.ui.ac.id/create-flutter/",
+                                "http://127.0.0.1:8000/create-flutter/",
                                 jsonEncode(<String, String>{
                                   'name': _name,
                                   'price': _price.toString(),
